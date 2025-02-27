36423322cec73b669b94639311527d4ce2e2e039
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2025-02-27 11:59:08 +0530

    Models

---
+++ models/task.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    is_completed: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", TaskSchema);
+module.exports = Task;
